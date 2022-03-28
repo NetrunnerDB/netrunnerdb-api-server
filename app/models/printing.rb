9d648aaa674ef36bde4c2678cd2031fe5f529c5f
Jason Gessner <jason@multiply.org>
2022-03-27 22:20:23 -0500

    Remove some unused methods in models.

--- app/models/printing.rb
+++ app/models/printing.rb
@@ -4,9 +4,4 @@ class Printing < ApplicationRecord
   belongs_to :card
   belongs_to :card_set
   # TODO(plural): Add an association to cycle.
-
-  def format_flavor
-    t = flavor || ''
-    "<p>#{t.split(/\n/).join('</p><p>')}</p>"
-  end
 end
