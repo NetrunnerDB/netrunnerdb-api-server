cdbae12e8f053ace1a2151a977fe4a0eb0cff597
distributive <github@ameliewd.com>
2022-07-03 22:38:51 +0100

    Snapshots now directly expose their card sets/cycles instead of linking to a card pool

--- app/resources/api/v3/public/snapshot_resource.rb
+++ app/resources/api/v3/public/snapshot_resource.rb
@@ -3,15 +3,23 @@ module API
     module Public
       class Api::V3::Public::SnapshotResource < JSONAPI::Resource
         immutable
-        
-        attributes :active, :date_start, :updated_at
+
+        attributes :active, :card_cycle_ids, :card_set_ids, :date_start, :updated_at
         key_type :string
 
         paginator :none
 
         belongs_to :format
-        has_one :card_pool
+        # has_one :card_pool
         has_one :restriction
+
+        def card_cycle_ids
+          @model.card_pool.card_pool_card_cycles.pluck(:card_cycle_id)
+        end
+
+        def card_set_ids
+          @model.card_pool.card_pool_card_sets.pluck(:card_set_id)
+        end
       end
     end
   end
