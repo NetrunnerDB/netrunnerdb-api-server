6e3e39f58d97eb0219298a81c0d3bcb30ff3d346
Jason Gessner <jason@multiply.org>
2022-07-17 12:42:47 -0500

    Add appropriate operators and some TODOs.

--- lib/search_query_builder.rb
+++ lib/search_query_builder.rb
@@ -1,96 +1,109 @@
 require 'search_parser'
-
 class SearchQueryBuilder
     @@string_keywords = [
         '_',
+        'card_type',
         'd',
         'f',
-        't',
-        'x',
-        'title',
-        'text',
-        'card_type',
         'faction',
         'side',
+        't',
+        'text',
+        'title',
+        'x',
     ]
     @@numeric_keywords = [
+        'advancement_cost',
+        'agenda_points',
+        'base_link',
+        'cost',
         'g',
         'h',
+        'influence_cost',
         'l',
         'm',
+        'memory_usage',
         'n',
         'o',
         'p',
-        'v',
-        'cost',
-        'advancement_cost',
-        'base_link',
-        'memory_usage',
-        'influence_cost',
         'strength',
-        'agenda_points',
         'trash_cost',
+        'v',
     ]
-    @@boolean_keywords = [ 'u', 'is_unique' ]
+    @@boolean_operators = {
+        ':' => '=',
+        '!' => '!=',
+    }
+    @@numeric_operators = {
+        ':' => '=',
+        '!' => '!=',
+        '<' => '<',
+        '<=' => '<=',
+        '>' => '>',
+        '>=' => '>='
+    }
+    @@string_operators = {
+        ':' => 'LIKE',
+        '!' => 'NOT LIKE',
+    }
+    @@boolean_keywords = [ 'is_unique', 'u' ]
     @@term_to_field_map = {
-        '_' => 'stripped_title',
-        # printing 'a' => 'flavor',
+        # 'card_pool' => ''',
+        # 'format' => ''',
+        # 'r' => 'release_date',
+        # 'restriction' => ''',
         # banlist 'b' => '',
+        # needs join table 'card_subtype' => ''',
+        # printing 'a' => 'flavor',
         # printing 'c' => 'card_cycle_id',
-        'd' => 'side_id',
+        # printing 'card_cycle' => 'card_cycle_id'',
+        # printing 'card_set' => 'card_set_id'',
         # printing 'e' => 'card_set_id',
+        # printing 'i' => 'illustrator',
+        # printing 'quantity_in_card_set' => ''',
+        # printing 'release_date' => ''',
+        # printing flavor 'flavor_text' => ''',
+        # printing illustrator 'illustrator' => ''',
+        # printing quantity 'y' => ''',
+        # rotation 'z' => ''',
+        # subtypes 's' => ''',
+        '_' => 'stripped_title',
+        'advancement_cost' => 'advancement_requirement',
+        'agenda_points' => 'agenda_points',
+        'base_link' => 'base_link',
+        'card_type' => 'card_type_id',
+        'cost' => 'cost',
+        'd' => 'side_id',
         'f' => 'faction_id',
+        'faction' => 'faction_id',
         'g' => 'advancement_requirement',
         'h' => 'trash_cost',
-        # printing 'i' => 'illustrator',
+        'influence_cost' => 'influence_cost',
+        'is_unique' => 'is_unique',
         'l' => 'base_link',
         'm' => 'memory_cost',
+        'memory_usage' => 'memory_cost',
         'n' => 'influence_cost',
         'o' => 'cost',
         'p' => 'strength',
-        # 'r' => 'release_date',
-        # subtypes 's' => ''',
+        'side' => 'card_side_id',
+        'strength' => 'strength',
         't' => 'card_type_id',
+        'text' => 'stripped_text',
+        'title' => 'stripped_title',
+        'trash_cost' => 'trash_cost',
         'u' => 'is_unique',
         'v' => 'agenda_points',
         'x' => 'stripped_text',
-        # printing quantity 'y' => ''',
-        # rotation 'z' => ''',
-        'title' => 'stripped_title',
-        'text' => 'stripped_text',
-        # printing flavor 'flavor_text' => ''',
-        # printing 'card_set' => 'card_set_id'',
-        # printing 'card_cycle' => 'card_cycle_id'',
-        'card_type' => 'card_type_id',
-        'faction' => 'faction_id',
-        # needs join table 'card_subtype' => ''',
-        'side' => 'card_side_id',
-        # printing illustrator 'illustrator' => ''',
-        'cost' => 'cost',
-        'advancement_cost' => 'advancement_requirement',
-        'base_link' => 'base_link',
-        'memory_usage' => 'memory_cost',
-        'influence_cost' => 'influence_cost',
-        'strength' => 'strength',
-        'agenda_points' => 'agenda_points',
-        'trash_cost' => 'trash_cost',
-        # printing 'release_date' => ''',
-        'is_unique' => 'is_unique',
-        # printing 'quantity_in_card_set' => ''',
-        # 'restriction' => ''',
-        # 'card_pool' => ''',
-        # 'format' => ''',
     }
-
     def initialize(query)
         @query = query
         @parse_error = nil
         # TODO(plural): Could be a singleton
         @parser = SearchParser.new
         @parse_tree = nil
-        @where = '' 
+        @where = ''
         @where_values = []
-
         begin
             @parse_tree = @parser.parse(@query)
         rescue Parslet::ParseFailed => e
@@ -103,26 +116,43 @@ class SearchQueryBuilder
         @parse_tree[:fragments].each {|f|
             if f.include?(:search_term)
                 if @@boolean_keywords.include?(f[:search_term][:keyword])
-                    constraints << '%s = ?' % [@@term_to_field_map[f[:search_term][:keyword].to_s]] 
+                    operator = ''
+                    if @@boolean_operators.include?(f[:search_term][:match_type].to_s)
+                        operator = @@boolean_operators[f[:search_term][:match_type].to_s]
+                    else
+                        # TODO(plural): throw an error earlier for invalid operator
+                    end
+                    constraints << '%s %s ?' % [@@term_to_field_map[f[:search_term][:keyword].to_s], operator]
                     @where_values << f[:search_term][:value][:string].to_s
-                elsif @@numeric_keywords.include?(f[:search_term][:keyword])
-                    constraints << '%s = ?' % [@@term_to_field_map[f[:search_term][:keyword].to_s]] 
+                elsif @@numeric_keywords.include?(f[:search_term][:keyword].to_s)
+                    operator = ''
+                    if @@numeric_operators.include?(f[:search_term][:match_type].to_s)
+                        operator = @@numeric_operators[f[:search_term][:match_type].to_s]
+                    else
+                        # TODO(plural): throw an error earlier for invalid operator
+                    end
+                    constraints << '%s %s ?' % [@@term_to_field_map[f[:search_term][:keyword].to_s], operator]
                     @where_values << f[:search_term][:value][:string].to_s
                 else
-                    constraints << 'lower(%s) LIKE ?' % [@@term_to_field_map[f[:search_term][:keyword].to_s]] 
+                    # String fields only support : and !
+                    operator = ''
+                    if @@string_operators.include?(f[:search_term][:match_type].to_s)
+                        operator = @@string_operators[f[:search_term][:match_type].to_s]
+                    else
+                        # TODO(plural): throw an error earlier for invalid operator
+                    end
+                    constraints << 'lower(%s) %s ?' % [@@term_to_field_map[f[:search_term][:keyword].to_s], operator]
                     @where_values << '%%%s%%' % f[:search_term][:value][:string].to_s
                 end
             end
-            # bare/quoted words in the query are automatically mapped to title 
+            # bare/quoted words in the query are automatically mapped to stripped_title
             if f.include?(:string)
                     constraints << 'lower(stripped_title) LIKE ?'
                     @where_values << '%%%s%%' % f[:string].to_s
-
             end
         }
         @where = constraints.join(' AND ')
     end
-
     def parse_error
         return @parse_error
     end
--- test/unit/search_query_builder_test.rb
+++ test/unit/search_query_builder_test.rb
@@ -22,6 +22,60 @@ class SearchQueryBuilderTest < Minitest::Test
     assert_equal ['%trash%', '3'], builder.where_values
   end
 
+  def test_numeric_field_not_equal 
+    input = %Q{trash_cost!3}
+    builder = SearchQueryBuilder.new(input)
+
+    assert_nil builder.parse_error
+    assert_equal 'trash_cost != ?', builder.where 
+    assert_equal ['3'], builder.where_values
+  end
+
+  def test_numeric_field_less_than
+    input = %Q{trash_cost<3}
+    builder = SearchQueryBuilder.new(input)
+
+    assert_nil builder.parse_error
+    assert_equal 'trash_cost < ?', builder.where 
+    assert_equal ['3'], builder.where_values
+  end
+
+  def test_numeric_field_less_than_equal_to
+    input = %Q{trash_cost<=3}
+    builder = SearchQueryBuilder.new(input)
+
+    assert_nil builder.parse_error
+    assert_equal 'trash_cost <= ?', builder.where 
+    assert_equal ['3'], builder.where_values
+  end
+
+  def test_numeric_field_greater_than
+    input = %Q{trash_cost>3}
+    builder = SearchQueryBuilder.new(input)
+
+    assert_nil builder.parse_error
+    assert_equal 'trash_cost > ?', builder.where 
+    assert_equal ['3'], builder.where_values
+  end
+
+  def test_numeric_field_greater_than_equal_to
+    input = %Q{trash_cost>=3}
+    builder = SearchQueryBuilder.new(input)
+
+    assert_nil builder.parse_error
+    assert_equal 'trash_cost >= ?', builder.where 
+    assert_equal ['3'], builder.where_values
+  end
+
+  def test_string_field_not_like
+    input = %Q{title!sure}
+    builder = SearchQueryBuilder.new(input)
+
+    assert_nil builder.parse_error
+    assert_equal 'lower(stripped_title) NOT LIKE ?', builder.where 
+    assert_equal ['%sure%'], builder.where_values
+  end
+
   def test_bare_word 
     input = %Q{diversion}
     builder = SearchQueryBuilder.new(input)
