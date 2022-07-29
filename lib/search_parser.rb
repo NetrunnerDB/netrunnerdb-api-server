3eaa600e7ebce2502efc321a2b78d80503456bf5
Jason Gessner <jason@multiply.org>
2022-07-28 23:53:21 -0500

    Format search_parser and add warning about clobbering.

--- lib/search_parser.rb
+++ lib/search_parser.rb
@@ -15,17 +15,39 @@ class SearchParser < Parslet::Parser
   rule(:string) {
     spaces? >> (bare_string | quoted_string) >> spaces?
   }
+  # Note that while this list should generally be kept sorted, an entry that is a prefix of
+  # a later entry will clobber the later entries and throw an error parsing text with the later entries.
   rule(:keyword) {
-    str('restriction_id') | str('is_restricted') | str('is_banned') |
-    str('eternal_points') | str('global_penalty') | str('universal_faction_cost') |
-    str('title') | str('text') | str('flavor_text') |
-    str('card_set') | str('card_cycle') | str('card_type') |
-    str('faction') | str('card_subtype') | str('side') |
-    str('illustrator') | str('cost') | str('advancement_cost') |
-    str('base_link') | str('memory_usage') | str('influence_cost') |
-    str('strength') | str('agenda_points') | str('trash_cost') |
-    str('release_date') | str('is_unique') | str('quantity_in_card_set') |
-    str('restriction') | str('card_pool') | str('format') |
+    str('advancement_cost') |
+    str('agenda_points') |
+    str('base_link') |
+    str('card_cycle') |
+    str('card_pool') |
+    str('card_set') |
+    str('card_subtype') |
+    str('card_type') |
+    str('cost') |
+    str('eternal_points') |
+    str('faction') |
+    str('flavor_text') |
+    str('format') |
+    str('global_penalty') |
+    str('illustrator') |
+    str('influence_cost') |
+    str('is_banned') |
+    str('is_restricted') |
+    str('is_unique') |
+    str('memory_usage') |
+    str('quantity_in_card_set') |
+    str('release_date') |
+    str('restriction_id') |
+    str('side') |
+    str('strength') |
+    str('text') |
+    str('title') |
+    str('trash_cost') |
+    str('universal_faction_cost') |
+    # Single letter 'short codes'
     match('[_abcdefghilmnoprstuvxyz]')
   }
   rule(:match_type) { str('<=') | str('>=') | match('[:!<>]') }
