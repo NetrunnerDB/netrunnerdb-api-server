cbac332e00211b44d9bcdade32d64794746fb599
distributive <github@ameliewd.com>
2022-09-21 20:37:28 +0100

    Exposed active card pool ID in formats

--- app/resources/api/v3/public/format_resource.rb
+++ app/resources/api/v3/public/format_resource.rb
@@ -4,9 +4,8 @@ module API
       class Api::V3::Public::FormatResource < JSONAPI::Resource
         immutable
 
-        attributes :name, :active_snapshot_id, :snapshot_ids
-        attribute :active_restriction_id
-        attribute :updated_at
+        attributes :name, :active_snapshot_id, :snapshot_ids, :updated_at
+        attributes :active_card_pool_id, :active_restriction_id
         key_type :string
 
         paginator :none
@@ -14,6 +13,10 @@ module API
         has_many :card_pools
         has_many :restrictions
         has_many :snapshots
+
+        def active_card_pool_id
+          @model.snapshots.find_by(active: true).card_pool_id
+        end
 
         def active_restriction_id
           @model.snapshots.find_by(active: true).restriction_id
