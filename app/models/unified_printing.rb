262148ea575ec48d84333d3eca285f4c9e2d1bd9
Jason Gessner <jason@multiply.org>
2022-11-01 22:50:06 -0700

    Add relationships for unified printing.

--- app/models/unified_printing.rb
+++ app/models/unified_printing.rb
@@ -1,3 +1,19 @@
 class UnifiedPrinting < ApplicationRecord
-    self.primary_key = :id 
+  self.primary_key = :id 
+
+  belongs_to :side
+  belongs_to :unified_card,
+    :primary_key => :id,
+    :foreign_key => :card_id
+  belongs_to :card
+  belongs_to :card_set
+  has_one :faction, :through => :card
+  has_one :card_cycle, :through => :card_set
+  has_one :side, :through => :card
+  has_many :illustrator_printings, primary_key: :id, foreign_key: :printing_id
+  has_many :illustrators, :through => :illustrator_printings
+
+  has_many :unified_restrictions, primary_key: :card_id, foreign_key: :card_id
+  has_many :card_pool_cards, primary_key: :card_id, foreign_key: :card_id
+  has_many :card_pools, :through => :card_pool_cards
 end
--- app/resources/api/v3/public/printing_resource.rb
+++ app/resources/api/v3/public/printing_resource.rb
@@ -4,6 +4,10 @@ module API
       class Api::V3::Public::PrintingResource < JSONAPI::Resource
         immutable
 
+        model_name 'UnifiedPrinting'
+
+        key_type :string
+
         # Direct printing attributes
         attributes :card_id, :card_set_id, :printed_text, :stripped_printed_text
         attributes :printed_is_unique, :flavor, :display_illustrators, :position
@@ -19,82 +23,80 @@ module API
         # Synthesized attributes
         attributes :images
 
-        key_type :string
-
-        has_one :card
+        has_one :card, relation_name: :unified_card
         has_one :card_cycle
         has_one :card_set
         has_one :faction
         has_many :illustrators
         has_one :side
-
-        # Printing direct attribute filters
-        filters :card_id, :card_set_id, :printed_is_unique, :display_illustrators, :position
-        filters :quantity, :date_release
-
-        # Card attribute filters
-        filter :title, apply: ->(records, value, _options){
-          Rails.logger.info(_options)
-          records.joins(:card).where('cards.title = ?', value)
-        }
-        filter :card_type_id, apply: ->(records, value, _options){
-          records.joins(:card).where('cards.card_type_id = ?', value)
-        }
-        filter :side_id, apply: ->(records, value, _options){
-          records.joins(:card).where('cards.side_id = ?', value)
-        }
-        filter :faction_id, apply: ->(records, value, _options){
-          records.joins(:card).where('cards.faction_id= ?', value)
-        }
-
-        filter :advancement_requirement, apply: ->(records, value, _options){
-          records.joins(:card).where('cards.advancement_requirement = ?', value)
-        }
-        filter :agenda_points, apply: ->(records, value, _options){
-          records.joins(:card).where('cards.agenda_points = ?', value)
-        }
-        filter :base_link, apply: ->(records, value, _options){
-          records.joins(:card).where('cards.base_link = ?', value)
-        }
-        filter :cost, apply: ->(records, value, _options){
-          records.joins(:card).where('cards.cost= ?', value)
-        }
-        filter :deck_limit, apply: ->(records, value, _options){
-          records.joins(:card).where('cards.deck_limit= ?', value)
-        }
-        filter :influence_cost, apply: ->(records, value, _options){
-          records.joins(:card).where('cards.influence_cost= ?', value)
-        }
-        filter :influence_limit, apply: ->(records, value, _options){
-          records.joins(:card).where('cards.influence_limit= ?', value)
-        }
-        filter :memory_cost, apply: ->(records, value, _options){
-          records.joins(:card).where('cards.memory_cost= ?', value)
-        }
-        filter :minimum_deck_size, apply: ->(records, value, _options){
-          records.joins(:card).where('cards.minimum_deck_size= ?', value)
-        }
-        filter :strength, apply: ->(records, value, _options){
-          records.joins(:card).where('cards.strength= ?', value)
-        }
-        filter :trash_cost, apply: ->(records, value, _options){
-          records.joins(:card).where('cards.trash_cost= ?', value)
-        }
-        filter :is_unique, apply: ->(records, value, _options){
-          records.joins(:card).where('cards.is_unique= ?', value)
-        }
-        filter :search, apply: ->(records, value, _options) {
-          query_builder = PrintingSearchQueryBuilder.new(value[0])
-          if query_builder.parse_error.nil?
-              records.left_joins(query_builder.left_joins)
-                  .where(query_builder.where, *query_builder.where_values)
-                  .distinct
-          else
-            raise JSONAPI::Exceptions::BadRequest.new(
-                'Invalid search query: [%s] / %s' % [value[0], query_builder.parse_error])
-          end
-        }
- 
+#
+#        # Printing direct attribute filters
+#        filters :card_id, :card_set_id, :printed_is_unique, :display_illustrators, :position
+#        filters :quantity, :date_release
+#
+#        # Card attribute filters
+#        filter :title, apply: ->(records, value, _options){
+#          Rails.logger.info(_options)
+#          records.joins(:card).where('cards.title = ?', value)
+#        }
+#        filter :card_type_id, apply: ->(records, value, _options){
+#          records.joins(:card).where('cards.card_type_id = ?', value)
+#        }
+#        filter :side_id, apply: ->(records, value, _options){
+#          records.joins(:card).where('cards.side_id = ?', value)
+#        }
+#        filter :faction_id, apply: ->(records, value, _options){
+#          records.joins(:card).where('cards.faction_id= ?', value)
+#        }
+#
+#        filter :advancement_requirement, apply: ->(records, value, _options){
+#          records.joins(:card).where('cards.advancement_requirement = ?', value)
+#        }
+#        filter :agenda_points, apply: ->(records, value, _options){
+#          records.joins(:card).where('cards.agenda_points = ?', value)
+#        }
+#        filter :base_link, apply: ->(records, value, _options){
+#          records.joins(:card).where('cards.base_link = ?', value)
+#        }
+#        filter :cost, apply: ->(records, value, _options){
+#          records.joins(:card).where('cards.cost= ?', value)
+#        }
+#        filter :deck_limit, apply: ->(records, value, _options){
+#          records.joins(:card).where('cards.deck_limit= ?', value)
+#        }
+#        filter :influence_cost, apply: ->(records, value, _options){
+#          records.joins(:card).where('cards.influence_cost= ?', value)
+#        }
+#        filter :influence_limit, apply: ->(records, value, _options){
+#          records.joins(:card).where('cards.influence_limit= ?', value)
+#        }
+#        filter :memory_cost, apply: ->(records, value, _options){
+#          records.joins(:card).where('cards.memory_cost= ?', value)
+#        }
+#        filter :minimum_deck_size, apply: ->(records, value, _options){
+#          records.joins(:card).where('cards.minimum_deck_size= ?', value)
+#        }
+#        filter :strength, apply: ->(records, value, _options){
+#          records.joins(:card).where('cards.strength= ?', value)
+#        }
+#        filter :trash_cost, apply: ->(records, value, _options){
+#          records.joins(:card).where('cards.trash_cost= ?', value)
+#        }
+#        filter :is_unique, apply: ->(records, value, _options){
+#          records.joins(:card).where('cards.is_unique= ?', value)
+#        }
+#        filter :search, apply: ->(records, value, _options) {
+#          query_builder = PrintingSearchQueryBuilder.new(value[0])
+#          if query_builder.parse_error.nil?
+#              records.left_joins(query_builder.left_joins)
+#                  .where(query_builder.where, *query_builder.where_values)
+#                  .distinct
+#          else
+#            raise JSONAPI::Exceptions::BadRequest.new(
+#                'Invalid search query: [%s] / %s' % [value[0], query_builder.parse_error])
+#          end
+#        }
+# 
         # Images will return a nested map for different types of images.
         # 'nrdb_classic' represents the JPEGs used for classic netrunnerdb.com.
         # We will likely add other formats like png and webp, as well as various sizes,
@@ -114,80 +116,21 @@ module API
             "large" => "%s/large/%s.jpg" % [url_prefix, @model.id]
           }
         end
-        def advancement_requirement
-          @model.card.advancement_requirement
-        end
-        def agenda_points
-          @model.card.agenda_points
-        end
-        def base_link
-          @model.card.base_link
-        end
+
         def card_abilities
           {
-            additional_cost: @model.card.additional_cost,
-            advanceable: @model.card.advanceable,
-            gains_subroutines: @model.card.gains_subroutines,
-            interrupt: @model.card.interrupt,
-            link_provided: @model.card.link_provided,
-            mu_provided: @model.card.mu_provided,
-            num_printed_subroutines: @model.card.num_printed_subroutines,
-            on_encounter_effect: @model.card.on_encounter_effect,
-            performs_trace: @model.card.performs_trace,
-            recurring_credits_provided: @model.card.recurring_credits_provided,
-            trash_ability: @model.card.trash_ability,
+            additional_cost: @model.additional_cost,
+            advanceable: @model.advanceable,
+            gains_subroutines: @model.gains_subroutines,
+            interrupt: @model.interrupt,
+            link_provided: @model.link_provided,
+            mu_provided: @model.mu_provided,
+            num_printed_subroutines: @model.num_printed_subroutines,
+            on_encounter_effect: @model.on_encounter_effect,
+            performs_trace: @model.performs_trace,
+            recurring_credits_provided: @model.recurring_credits_provided,
+            trash_ability: @model.trash_ability,
           }
-        end
-        def card_type_id
-          @model.card.card_type_id
-        end
-        def cost
-          @model.card.cost
-        end
-        def deck_limit
-          @model.card.deck_limit
-        end
-        def display_subtypes
-          @model.card.display_subtypes
-        end
-        def faction_id
-          @model.card.faction_id
-        end
-        def influence_cost
-          @model.card.influence_cost
-        end
-        def influence_limit
-          @model.card.influence_limit
-        end
-        def is_unique
-          @model.card.is_unique
-        end
-        def memory_cost
-          @model.card.memory_cost
-        end
-        def minimum_deck_size
-          @model.card.minimum_deck_size
-        end
-        def side_id
-          @model.card.side_id
-        end
-        def strength
-          @model.card.strength
-        end
-        def stripped_text
-          @model.card.stripped_text
-        end
-        def stripped_title
-          @model.card.stripped_title
-        end
-        def text
-          @model.card.text
-        end
-        def title
-          @model.card.title
-        end
-        def trash_cost
-          @model.card.trash_cost
         end
       end
     end
