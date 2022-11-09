2cc5adb0b882c6f106a2857fba215309a3ea0510
Jason Gessner <jason@multiply.org>
2022-11-08 20:07:35 -0600

    Fix card subtypes relationship for unified cards.

--- app/models/unified_card.rb
+++ app/models/unified_card.rb
@@ -3,6 +3,12 @@ class UnifiedCard < ApplicationRecord
 
     self.primary_key = :id
 
+    has_many :card_card_subtypes,
+      :primary_key => :id,
+      :foreign_key => :card_id
+
+    has_many :card_subtypes, :through => :card_card_subtypes
+
     has_many :unified_printings,
       :primary_key => :id,
       :foreign_key => :card_id
