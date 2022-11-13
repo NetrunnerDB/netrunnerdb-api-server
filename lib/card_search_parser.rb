0ab8a1dcd6fc2dc14b76d3ee75786adb86d8584b
distributive <github@ameliewd.com>
2022-11-13 00:33:58 +0000

    Removed keyword checks from parser

--- lib/card_search_parser.rb
+++ lib/card_search_parser.rb
@@ -1,4 +1,5 @@
 require 'parslet'
+# require_relative "search_parser"
 
 class CardSearchParser < Parslet::Parser
   rule(:spaces) { match('\s').repeat(1) }
@@ -31,53 +32,7 @@ class CardSearchParser < Parslet::Parser
 
   # Note that while this list should generally be kept sorted, an entry that is a prefix of
   # a later entry will clobber the later entries and throw an error parsing text with the later entries.
-  rule(:keyword) {
-    str('additional_cost') |
-    str('advanceable') |
-    str('advancement_cost') |
-    str('agenda_points') |
-    str('attribution') |
-    str('base_link') |
-    str('card_cycle') |
-    str('card_pool') |
-    str('card_set') |
-    str('card_subtype_id') |
-    str('card_subtype') |
-    str('card_type') |
-    str('cost') |
-    str('eternal_points') |
-    str('faction') |
-    str('format') |
-    str('gains_subroutines') |
-    str('has_global_penalty') |
-    str('illustrator') |
-    str('in_restriction') |
-    str('influence_cost') |
-    str('interrupt') |
-    str('is_banned') |
-    str('is_restricted') |
-    str('is_unique') |
-    str('link_provided') |
-    str('memory_usage') |
-    str('mu_provided') |
-    str('num_printed_subroutines') |
-    str('num_printings') |
-    str('on_encounter_effect') |
-    str('performs_trace') |
-    str('printing_id') |
-    str('recurring_credits_provided') |
-    str('restriction_id') |
-    str('side') |
-    str('snapshot') |
-    str('strength') |
-    str('text') |
-    str('title') |
-    str('trash_ability') |
-    str('trash_cost') |
-    str('universal_faction_cost') |
-    # Single letter 'short codes'
-    match('[_abcdefghilmnoprstuvxyz]')
-  }
+  rule(:keyword) { match('[_abcdefghilmnoprstuvxyz]').repeat(1) }
 
   rule(:pair) { keyword.as(:keyword) >> operator.as(:operator) >> value_ors.as(:values) }
   rule(:operator) { str('<=') | str('>=') | match('[:!<>]') }
