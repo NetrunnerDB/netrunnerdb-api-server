3b8fc141704ca71390e68b15c96863f79f8a9e36
plural <jason@multiply.org>
2023-06-03 21:17:57 +0000

    Protect DeckController with JwtAuthorizationConcern.
    Add current_user to context.

--- app/controllers/api/v3/private/decks_controller.rb
+++ app/controllers/api/v3/private/decks_controller.rb
@@ -2,7 +2,11 @@ module API
   module V3
     module Private
       class Api::V3::Private::DecksController < JSONAPI::ResourceController
-        # include JwtAuthorizationConcern
+        include JwtAuthorizationConcern
+
+        def context
+          {current_user: @current_user}
+        end
       end
     end
   end
