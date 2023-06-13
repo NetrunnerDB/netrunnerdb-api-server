41b292fae5251a5062de18cc05331e3239db9d1d
Jason Gessner <jason@multiply.org>
2023-06-12 23:59:04 -0500

    Add code coverage to rails test.

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -2,6 +2,12 @@ ENV["RAILS_ENV"] ||= "test"
 require_relative "../config/environment"
 require "rails/test_help"
 
+require "simplecov"
+SimpleCov.start
+
+require 'simplecov-cobertura'
+SimpleCov.formatter = SimpleCov::Formatter::CoberturaFormatter
+
 class ActiveSupport::TestCase
   # Run tests in parallel with specified workers
   parallelize(workers: :number_of_processors)
