8caa14dad69ff3be13bade16adb28b86c0412c8c
Jason Gessner <jason@multiply.org>
2023-07-04 16:24:55 -0500

    Uncomment basic tests and ensure they pass.

--- test/unit/deck_validator_test.rb
+++ test/unit/deck_validator_test.rb
@@ -8,11 +8,11 @@ class DeckValidatorTest < ActiveSupport::TestCase
     @imaginary_identity = { identity_card_id: 'plural', side_id: 'corp' }
     @imaginary_side = { identity_card_id: 'geist', side_id: 'super_mega_corp' }
 
-    @minimal_asa_group = { identity_card_id: 'asa_group', side_id: 'corp', cards: { 'hedge_fund': 3} }
-    @minimal_geist = { identity_card_id: 'geist', side_id: 'runner', cards: { 'sure_gamble': 3} }
-
     @wrong_side_asa_group = { identity_card_id: 'asa_group', side_id: 'runner' }
     @wrong_side_geist = { identity_card_id: 'geist', side_id: 'corp' }
+
+    @minimal_asa_group = { identity_card_id: 'asa_group', side_id: 'corp', cards: { 'hedge_fund': 3} }
+    @minimal_geist = { identity_card_id: 'geist', side_id: 'runner', cards: { 'sure_gamble': 3} }
   end
 
   def test_empty_deck_json
@@ -28,30 +28,29 @@ class DeckValidatorTest < ActiveSupport::TestCase
     assert_includes v.errors, "Deck is missing `identity_card_id` field."
   end
 
-#  def test_missing_side
-#    v = DeckValidator.new()
-#    assert !v.validate(@missing_side), 'Deck JSON missing side fails validation'
-#    assert_includes v.errors, "Deck is missing `side_id` field."
-#  end
-#
-#  def test_imaginary_identity
-#    v = DeckValidator.new()
-#    assert !v.validate(@imaginary_identity), 'Deck JSON has non-existent Identity'
-#    assert_includes v.errors, "`identity_card_id` `plural` does not exist."
-#  end
-#
-#  def test_imaginary_side
-#    v = DeckValidator.new()
-#    assert !v.validate(@imaginary_side), 'Deck JSON has non-existent side'
-#    assert_includes v.errors, "`side_id` `super_mega_corp` does not exist."
-#  end
-#
-#  def test_mismatched_side_corp_id
-#    v = DeckValidator.new()
-#    assert !v.validate(@wrong_side_asa_group), 'Deck with mismatched id and specified side fails'
-#    assert_equal 1, v.errors.size
-#    assert_includes v.errors, "Identity `asa_group` has side `corp` which does not match given side `runner`"
-#  end
+  def test_missing_side
+    v = DeckValidator.new()
+    assert !v.validate(@missing_side), 'Deck JSON missing side fails validation'
+    assert_includes v.errors, "Deck is missing `side_id` field."
+  end
+
+  def test_imaginary_identity
+    v = DeckValidator.new()
+    assert !v.validate(@imaginary_identity), 'Deck JSON has non-existent Identity'
+    assert_includes v.errors, "`identity_card_id` `plural` does not exist."
+  end
+
+  def test_imaginary_side
+    v = DeckValidator.new()
+    assert !v.validate(@imaginary_side), 'Deck JSON has non-existent side'
+    assert_includes v.errors, "`side_id` `super_mega_corp` does not exist."
+  end
+
+  def test_mismatched_side_corp_id
+    v = DeckValidator.new()
+    assert !v.validate(@wrong_side_asa_group), 'Deck with mismatched id and specified side fails'
+    assert_includes v.errors, "Identity `asa_group` has side `corp` which does not match given side `runner`"
+  end
 
   def test_mismatched_side_runner_id
     v = DeckValidator.new()
