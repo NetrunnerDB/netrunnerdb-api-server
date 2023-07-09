a148ad7928c47bdee87565aea891e1ab4bfdb5fd
Jason Gessner <jason@multiply.org>
2023-07-09 15:41:12 -0500

    Update errors to validation_errors for a valid request.

--- app/controllers/api/v3/public/validate_deck_controller.rb
+++ app/controllers/api/v3/public/validate_deck_controller.rb
@@ -5,6 +5,7 @@ module API
           def index
             out = params[:data]
 
+            # Check for presence of everything needed to perform deck validation and error if not present.
             if out.nil? or not (out.has_key?(:attributes) and out[:attributes].has_key?(:identity_card_id) and out[:attributes].has_key?(:side_id) and out[:attributes].has_key?(:cards))
               return render json: {
                 :errors => [{
@@ -16,6 +17,7 @@ module API
                 }]}, :status => :bad_request
             end
 
+            # Deck validation works off of a simple datastructure, so construct it instead of passing around ActionController::Parameters
             deck = {
               'identity_card_id' => params[:data][:attributes][:identity_card_id],
               'side_id' => params[:data][:attributes][:side_id],
@@ -26,8 +28,8 @@ module API
             v = DeckValidator.new(deck)
 
             out[:attributes][:is_valid] = v.is_valid?
-            out[:attributes][:errors] = v.errors
-            render json: { data: out }
+            out[:attributes][:validation_errors] = v.errors
+            render json: { data: out }, :status => :ok
           end
         end
       end
