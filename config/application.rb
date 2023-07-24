26e4fb364d6a95e5cd2bd436721e6b910dda3031
Jason Gessner <jason@multiply.org>
2023-07-23 20:01:49 -0500

    Move to the DigitalOcean space for image serving.

--- config/application.rb
+++ config/application.rb
@@ -27,7 +27,7 @@ module NrdbApi
     config.api_only = true
 
     # Prefix for Printing images.
-    config.x.printing_images.nrdb_classic_prefix = 'https://static.nrdbassets.com/v1'
+    config.x.printing_images.nrdb_classic_prefix = 'https://card-images.netrunnerdb.com/v1'
 
     config.encoding = 'utf-8'
   end
