5ac8b2caabd79d23105d175ddc885313ab5f576c
Jason Gessner <jason@multiply.org>
2023-08-26 16:08:31 -0500

    Run tests serially and move materialized views to test_helper.

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -18,11 +18,11 @@ SimpleCov.start do
 end
 
 class ActiveSupport::TestCase
-  # Run tests in parallel with specified workers
-  parallelize(workers: :number_of_processors)
-
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
   fixtures :all
 
   # Add more helper methods to be used by all tests here...
+  Scenic.database.refresh_materialized_view(:unified_restrictions, concurrently: false, cascade: false)
+  Scenic.database.refresh_materialized_view(:unified_cards, concurrently: false, cascade: false)
+  Scenic.database.refresh_materialized_view(:unified_printings, concurrently: false, cascade: false)
 end
