3a07d709ff95e084e274c772f34cf67774fefba6
Jason Gessner <jason@multiply.org>
2024-02-18 22:37:17 -0600

    Disable Open Telemetry for now.

--- config/initializers/opentelemetry.rb
+++ config/initializers/opentelemetry.rb
@@ -3,7 +3,8 @@ require 'opentelemetry/exporter/otlp'
 require 'opentelemetry/instrumentation/all'
 
 OpenTelemetry::SDK.configure do |c|
-    if ['production', 'development'].include?(ENV['RAILS_ENV'])
-        c.use_all() # enables all instrumentation!
-    end
+# Disable this for now.
+#    if ['production', 'development'].include?(ENV['RAILS_ENV'])
+#        c.use_all() # enables all instrumentation!
+#    end
 end
