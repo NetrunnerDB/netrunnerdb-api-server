8011b13d513f2e3307ab07b0fa4306cefa66dd8e
Jason Gessner <jason@multiply.org>
2024-04-13 00:45:10 -0500

    Add id and side relationships to decklist.

--- app/models/decklist.rb
+++ app/models/decklist.rb
@@ -1,4 +1,7 @@
 class Decklist < ApplicationRecord
+  has_one :identity_card, :class_name => 'Card', :foreign_key => 'id', :primary_key => 'identity_card_id'
+  belongs_to :side
+
   has_many :decklist_cards, dependent: :destroy
   has_many :cards, :through => :decklist_cards
 end
