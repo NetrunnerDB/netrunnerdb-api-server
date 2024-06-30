c757e352db3caff9ebbcb4c81935e18f0e648de8
Jason Gessner <jason@multiply.org>
2024-06-30 14:13:09 +0000

    Update private user resource and controller.

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,8 +1,10 @@
 # frozen_string_literal: true
 
-# Controller for the Snapshot resource.
+# Controller for the private User resource, will force this to only return the current user.
 class UserController < ApplicationController
-    def index
-      include JwtAuthorizationConcern
-    end
+  include JwtAuthorizationConcern
+  def index
+    user = UserResource.find({ id: current_user.id })
+    respond_with(user)
+  end
 end
