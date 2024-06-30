0880c0a1cc751e1c7dc6959583250366ebb9d230
Jason Gessner <jason@multiply.org>
2024-06-30 05:12:40 +0000

    Update JWT Concern and add error classes.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,4 +3,10 @@
 # Base controller for NRDB API Resources.
 class ApplicationController < ActionController::API
   include Graphiti::Rails::Responders
+
+  class UnauthenticatedError < StandardError; end
+  class UnauthorizedError < StandardError; end
+
+  register_exception UnauthenticatedError, status: 401
+  register_exception UnauthorizedError, status: 403
 end
