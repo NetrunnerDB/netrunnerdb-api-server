21946ac18722c856e25b9124d8a3bcea5feb452e
Jason Gessner <jason@multiply.org>
2024-07-01 02:29:08 +0000

    Remove JSONAPI controllers and resources.

--- app/controllers/api/v3/public/card_sets_controller.rb
+++
@@ -1,8 +0,0 @@
-module API
-  module V3
-    module Public
-      class Api::V3::Public::CardSetsController < Api::V3::Public::PublicController
-      end
-    end
-  end
-end
--- app/controllers/api/v3/public/formats_controller.rb
+++
@@ -1,8 +0,0 @@
-module API
-  module V3
-    module Public
-      class Api::V3::Public::FormatsController < Api::V3::Public::PublicController
-      end
-    end
-  end
-end
