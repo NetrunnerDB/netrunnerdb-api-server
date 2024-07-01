c2ae0743591a26405c4243faa58849ca28efa700
Jason Gessner <jason@multiply.org>
2024-07-01 02:43:26 +0000

    Remove commented out routes.

--- config/routes.rb
+++ config/routes.rb
@@ -31,32 +31,4 @@ Rails.application.routes.draw do
     resources :user, only: %i[index]
     resources :decks
   end
-   # namespace :api do
-  #   namespace :v3 do
-  #     namespace :private, defaults: { format: :json } do
-  #       # Don't generate links or relationship routes for decks.
-  #       jsonapi_resources :decks do
-  #       end
-  #       jsonapi_resources :user, only: [:index, :show]
-  #     end
-  #     namespace :public, defaults: { format: :json } do
-  #       jsonapi_resources :card_cycles, only: [:index, :show]
-  #       jsonapi_resources :card_pools, only: [:index, :show]
-  #       jsonapi_resources :card_set_types, only: [:index, :show]
-  #       jsonapi_resources :card_sets, only: [:index, :show]
-  #       jsonapi_resources :card_subtypes, only: [:index, :show]
-  #       jsonapi_resources :card_types, only: [:index, :show]
-  #       jsonapi_resources :cards, only: [:index, :show]
-  #       jsonapi_resources :decklists, only: [:index, :show]
-  #       jsonapi_resources :factions, only: [:index, :show]
-  #       jsonapi_resources :formats, only: [:index, :show]
-  #       jsonapi_resources :illustrators, only: [:index, :show]
-  #       jsonapi_resources :printings, only: [:index, :show]
-  #       jsonapi_resources :restrictions, only: [:index, :show]
-  #       jsonapi_resources :rulings, only: [:index]
-  #       jsonapi_resources :sides, only: [:index, :show]
-  #       jsonapi_resources :snapshots, only: [:index, :show]
-  #     end
-  #   end
-  # end
 end
