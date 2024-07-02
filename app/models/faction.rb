0d50dee88c92220ff70cdd33027fb4973c30bd05
Jason Gessner <jason@multiply.org>
2024-07-02 05:13:50 +0000

    Remove TODO for faction -> deck model relationshp

--- app/models/faction.rb
+++ app/models/faction.rb
@@ -3,7 +3,6 @@
 class Faction < ApplicationRecord
   belongs_to :side
   has_many :cards
-  # TODO(plural): Add decklist relationship
   has_many :printings, :through => :cards
   has_many :unified_cards
   has_many :unified_printings, :through => :cards
