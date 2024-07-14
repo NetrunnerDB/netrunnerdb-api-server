b4a75bc6cfaac026024141a2f61c349b761f88fc
Jason Gessner <jason@multiply.org>
2024-07-14 23:15:18 +0000

    Move logic from resource to decklist model, add spec test.

--- app/models/decklist.rb
+++ app/models/decklist.rb
@@ -16,4 +16,24 @@ class Decklist < ApplicationRecord
 
   has_many :decklist_cards
   has_many :cards, through: :decklist_cards
+
+  def card_slots
+    decklist_cards.order(:card_id).each_with_object({}) { |c, h| h[c.card_id] = c.quantity }
+  end
+
+  def num_cards
+    decklist_cards.map(&:quantity).sum
+  end
+
+  # TODO(plural): Extract this out to share between public and private decklists.
+  # This is the basic definition, but does not take restriction modifications
+  # into account. Leaving this here as an example for now, but it will need to
+  # be removed in favor of snapshot-specific calculations.
+  def influence_spent
+    qty = decklist_cards.each_with_object({}) { |c, h| h[c.card_id] = c.quantity }
+    cards
+      .filter { |c| c.faction_id != identity_card.faction_id }
+      .map { |c| c.influence_cost.nil? ? 0 : (c.influence_cost * qty[c.id]) }
+      .sum
+  end
 end
