3b2ce97c0e4dcfbb2ac4b0a53843ddabfe9df602
Jason Gessner <jason@multiply.org>
2024-07-14 04:41:37 +0000

    Resource specs for cycles and sets.

--- app/models/card_set.rb
+++ app/models/card_set.rb
@@ -4,6 +4,11 @@ class CardSet < ApplicationRecord
   belongs_to :card_cycle
   belongs_to :card_set_type
 
+  def first_printing_id
+    first_printing = Printing.find_by(card_set_id: id, position_in_set: 1)
+    first_printing&.id
+  end
+
   has_many :raw_printings
   has_many :printings
   has_many :raw_cards, through: :raw_printings, source: :raw_card
