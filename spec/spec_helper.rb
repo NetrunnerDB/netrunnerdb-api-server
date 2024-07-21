f8b074297ebffbf24a6f0090ee4969fce0ca8c2c
Jason Gessner <jason@multiply.org>
2024-07-21 16:35:30 +0000

    Combine coverage reports from unit and rspec tests.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -98,7 +98,7 @@ require 'rspec_api_documentation/dsl'
 RspecApiDocumentation.configure do |config|
   config.format = :json
 
-  config.request_headers_to_include = ['Host', 'Content-Type'] 
+  config.request_headers_to_include = ['Host', 'Content-Type']
   config.response_headers_to_include = ['Content-Type']
 
   config.curl_host = 'https://api-preview.netrunnerdb.com'
