d833c9cc797a124e8420f49a749e77c80e31666a
Jason Gessner <jason@multiply.org>
2024-11-24 05:43:16 +0000

    Optimize a few things for snapshots.

--- app/controllers/snapshots_controller.rb
+++ app/controllers/snapshots_controller.rb
@@ -3,14 +3,15 @@
 # Controller for the Snapshot resource.
 class SnapshotsController < ApplicationController
   def index
-    base_scope = Snapshot.includes(:card_pool, :cards, :restriction)
+    base_scope = Snapshot.includes(:card_pool)
     snapshots = SnapshotResource.all(params, base_scope)
 
     respond_with(snapshots)
   end
 
   def show
-    snapshot = SnapshotResource.find(params)
+    base_scope = Snapshot.includes(:card_pool)
+    snapshot = SnapshotResource.find(params, base_scope)
     respond_with(snapshot)
   end
 end
