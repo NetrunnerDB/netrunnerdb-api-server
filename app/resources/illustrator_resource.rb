6ebd8ac368e79db592c3e47f1e2a359e22ab0957
Jason Gessner <jason@multiply.org>
2024-12-13 03:39:43 +0000

    Fix relationship problem for illustrators <-> printings.

--- app/resources/illustrator_resource.rb
+++ app/resources/illustrator_resource.rb
@@ -4,10 +4,16 @@
 class IllustratorResource < ApplicationResource
   primary_endpoint '/illustrators', %i[index show]
 
+  self.attributes_writable_by_default = false
+
   attribute :id, :string
   attribute :name, :string
   attribute :num_printings, :integer
   attribute :updated_at, :datetime
 
-  many_to_many :printings, relation_name: :printings
+  many_to_many :printings do
+    assign_each do |illustrator, printings|
+      printings.select { |p| p.illustrator_ids_in_database.include?(illustrator.id) }
+    end
+  end
 end
