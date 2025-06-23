e3ed0a6fd60000e4ac0d1c762fba1d2058142078
talhaahsan <stwyde@gmail.com>
2025-06-23 17:34:16 +0000

    lint bump

--- db/migrate/20250623035759_update_unified_cards_to_version_10.rb
+++ db/migrate/20250623035759_update_unified_cards_to_version_10.rb
@@ -1,8 +1,10 @@
+# frozen_string_literal: true
+
 class UpdateUnifiedCardsToVersion10 < ActiveRecord::Migration[7.2]
   def change
     update_view :unified_cards,
-      version: 10,
-      revert_to_version: 9,
-      materialized: true
+                version: 10,
+                revert_to_version: 9,
+                materialized: true
   end
 end
--- db/migrate/20250623035928_update_unified_printings_to_version_12.rb
+++ db/migrate/20250623035928_update_unified_printings_to_version_12.rb
@@ -1,8 +1,10 @@
+# frozen_string_literal: true
+
 class UpdateUnifiedPrintingsToVersion12 < ActiveRecord::Migration[7.2]
   def change
     update_view :unified_printings,
-      version: 12,
-      revert_to_version: 11,
-      materialized: true
+                version: 12,
+                revert_to_version: 11,
+                materialized: true
   end
 end
