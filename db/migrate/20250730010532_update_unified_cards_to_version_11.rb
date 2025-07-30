2e1560d55624bab6fbead40e90e8ec03cf289cbe
Miguel de los Reyes <thisiswhataninjalookslike@gmail.com>
2025-07-30 01:09:26 +0000

    Fix linting

--- db/migrate/20250730010532_update_unified_cards_to_version_11.rb
+++ db/migrate/20250730010532_update_unified_cards_to_version_11.rb
@@ -1,8 +1,10 @@
-class UpdateUnifiedCardsToVersion11 < ActiveRecord::Migration[7.2]
+# frozen_string_literal: true
+
+class UpdateUnifiedCardsToVersion11 < ActiveRecord::Migration[7.2] # rubocop:disable Style/Documentation
   def change
     update_view :unified_cards,
-      version: 11,
-      revert_to_version: 10,
-      materialized: true
+                version: 11,
+                revert_to_version: 10,
+                materialized: true
   end
 end
--- db/migrate/20250730010551_update_unified_printings_to_version_13.rb
+++ db/migrate/20250730010551_update_unified_printings_to_version_13.rb
@@ -1,8 +1,10 @@
-class UpdateUnifiedPrintingsToVersion13 < ActiveRecord::Migration[7.2]
+# frozen_string_literal: true
+
+class UpdateUnifiedPrintingsToVersion13 < ActiveRecord::Migration[7.2] # rubocop:disable Style/Documentation
   def change
     update_view :unified_printings,
-      version: 13,
-      revert_to_version: 12,
-      materialized: true
+                version: 13,
+                revert_to_version: 12,
+                materialized: true
   end
 end
