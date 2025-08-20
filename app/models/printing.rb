7ba8eaef705608f76bdb1e998c744fe34761856b
Lia Huynh <tinlun123@gmail.com>
2025-08-20 02:09:07 -0700

    Use released_by instead of designed_by, excluding some sets. Add tests

--- app/models/printing.rb
+++ app/models/printing.rb
@@ -43,6 +43,16 @@ class Printing < ApplicationRecord
     nil
   end
 
+  def xlarge_image?
+    released_by == 'null_signal_games' &&
+      !%w[
+        system_core_2019
+        magnum_opus_reprint
+        salvaged_memories
+        system_update_2021
+      ].include?(card_set_id)
+  end
+
   def restrictions
     {
       banned: restrictions_banned,
